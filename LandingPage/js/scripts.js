7e18ec1db9921f51ed7984d267093cd52ff6c22a
Kamil Klamra <kk301986@student.polsl.pl>
2024-01-13 21:57:46 +0100

    2024-01-13 Small changes

--- LandingPage/js/scripts.js
+++ LandingPage/js/scripts.js
@@ -9,9 +9,12 @@ window.addEventListener('load', function() {
     //     });
 
     this.document.getElementById('find_button').addEventListener('click', () => {
-        window.location.href = '/find';
+        window.location.href = './find/index.html';
     });
     this.document.getElementById('user_profile').addEventListener('click', () => {
-        window.location.href = '/myaccount';
+        window.location.href = './myaccount/index.html';
+    });
+    this.document.getElementById('icon_tutorino').addEventListener('click', () => {
+        window.location.href = '../index.html';
     });
 });
